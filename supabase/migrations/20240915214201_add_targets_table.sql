88015ec47f1d96b0659ee42d268c7464f8b7dfc9
Johannes Naylor <jonaylor89@gmail.com>
2024-09-15 22:30:08 -0400

    supabase init

--- supabase/migrations/20240915214201_add_targets_table.sql
+++ supabase/migrations/20240915214201_add_targets_table.sql
@@ -1,8 +1,11 @@
-REPLACE OR CREATE TABLE targets (
-    id UUID NOT NULL,
+CREATE TABLE targets (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
     version INT NOT NULL DEFAULT 1,
     user_id UUID NOT NULL,
     name VARCHAR(255) NOT NULL,
+    initial_attrs JSONB NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id, version)
+    PRIMARY KEY (id, version),
+
+    FOREIGN KEY (user_id) REFERENCES auth.users(id)
 );
